d5673435946b919014717e50b38b212790728c33
kalin <kalindusupun@gmail.com>
2022-01-16 19:05:40 +0530

    MVVM-tutorials

---
+++ app/src/main/java/com/mvvm_tutorials/mvvm_movie_retrofit/ActivityMovieMain.java
@@ -0,0 +1,16 @@
+package com.mvvm_tutorials.mvvm_movie_retrofit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mvvm_tutorials.R;
+
+public class ActivityMovieMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_main);
+    }
+}
