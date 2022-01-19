2dfa9fa328bef6743bcde58fdd3935b92140887e
kalin <kalindusupun@gmail.com>
2022-01-19 10:35:53 +0530

    MVVM-tutorials

---
+++ app/src/main/java/com/mvvm_tutorials/mvvm_movie_retrofit/MovieSingleResponse.java
@@ -0,0 +1,26 @@
+package com.mvvm_tutorials.mvvm_movie_retrofit;
+
+
+import com.google.gson.annotations.SerializedName;
+
+//this class is for getting 1 movie data
+//https://api.themoviedb.org/3/movie/550?api_key=459351d1311a26f26c93016a0d788db7
+// id=550
+
+public class MovieSingleResponse {
+
+
+    @SerializedName("results")
+    private MovieModel results;
+
+    public MovieModel getResults() {
+        return results;
+    }
+
+    @Override
+    public String toString() {
+        return "MoviesIdResponse{" +
+                "results=" + results +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/mvvm_tutorials/mvvm_movie_retrofit/MoviesSearchResponse.java
@@ -0,0 +1,35 @@
+package com.mvvm_tutorials.mvvm_movie_retrofit;
+
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+//this class is for getting multiple movies in results array
+//https://api.themoviedb.org/3/movie/popular?api_key=459351d1311a26f26c93016a0d788db7
+
+public class MoviesSearchResponse {
+
+    @SerializedName("total_results")
+    private int total_results;
+
+    @SerializedName("results")
+    //@Expose() then you can uses different name for object
+    private List<MovieModel> results;
+
+    public int getTotal_results() {
+        return total_results;
+    }
+
+    public List<MovieModel> getResults() {
+        return results;
+    }
+
+    @Override
+    public String toString() {
+        return "MoviesResponse{" +
+                "total_results=" + total_results +
+                ", results=" + results +
+                '}';
+    }
+}
